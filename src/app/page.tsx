1b77b121f731ecc70f8248f08e9214a215cdb690
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-11-29 02:45:01 +0500

    bug fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import NavBar from "@/components/common/NavBar";
 import TopBar from "@/components/common/TopBar";
 import WebLayout from "@/components/layouts/WebLayout";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <TopBar />
       <NavBar />
-      <WebLayout/>
+      <WebLayout />
     </>
   );
 }

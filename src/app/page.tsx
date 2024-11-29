2f0f5c6e5258b42a29ff72fc3a80472ef6d993b3
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-11-29 10:56:52 +0500

    dynamic imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Banner from "@/components/home/Banner";
-import OurServices from "@/components/home/OurServices";
-import CapabilitiesOverview from "@/components/home/CapabilitiesOverview";
-import AboutRMT from "@/components/home/AboutRMT";
+import dynamic from "next/dynamic";
+
+// Dynamic imports
+const Banner = dynamic(() => import("@/components/home/Banner"));
+const AboutRMT = dynamic(() => import("@/components/home/AboutRMT"));
+const OurServices = dynamic(() => import("@/components/home/OurServices"));
+const CapabilitiesOverview = dynamic(
+  () => import("@/components/home/CapabilitiesOverview")
+);
 
 export default function Home() {
   return (

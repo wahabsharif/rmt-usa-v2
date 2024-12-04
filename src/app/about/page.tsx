df7ba03e78a996b7b5fd5e5ed33f21fe58475f82
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-12-05 02:02:22 +0500

    Google Map

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,7 @@ import AboutCompanyServices from "@/components/about/AboutCompanyServices";
 import AboutUsTwo from "@/components/about/AboutUsTwo";
 import Banner from "@/components/about/Banner";
 import MeetTeam from "@/components/about/MeetTeam";
+import GoogleMap from "@/components/common/GoogleMap";
 import Feature from "@/components/Feature";
 import React from "react";
 
@@ -15,6 +16,7 @@ const AboutUsPage: React.FC = () => {
       <AboutCompanyServices />
       <AboutUsTwo />
       <Feature />
+      <GoogleMap />
     </>
   );
 };

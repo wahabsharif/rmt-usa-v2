22ae8c066c5ba29a2c381b6fb70a9d910a6a6378
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-12-10 10:03:58 +0500

    minor bug

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,27 +1,26 @@
-
+import React from "react";
 import OurLeadership from "@/components/team/OurLeadership";
 import OurTeam from "@/components/team/OurTeam";
-import OurTeamClients from "@/components/team/OurTeamClients";
-import OurTeamFacility from "@/components/team/OurTeamFacility";
-import OurTeamImpact from "@/components/team/OurTeamImpact";
-import OurTeamValues from "@/components/team/OurTeamValues";
-import TeamAbout from "@/components/team/TeamAbout";
 import TeamBanner from "@/components/team/TeamBanner";
-import React from "react";
+
+// import OurTeamClients from "@/components/team/OurTeamClients";
+// import OurTeamFacility from "@/components/team/OurTeamFacility";
+// import OurTeamImpact from "@/components/team/OurTeamImpact";
+// import OurTeamValues from "@/components/team/OurTeamValues";
+// import TeamAbout from "@/components/team/TeamAbout";
 
 const Team: React.FC = () => {
-    return (
-        <>
-          <TeamBanner/>
-          
-          {/* <OurTeamImpact/> */}
-          <OurLeadership/>
-          <OurTeam/>
-          {/* <OurTeamValues/>
-          <OurTeamFacility/>
-          <OurTeamClients/> */}
-        </>
-    );
+  return (
+    <>
+      <TeamBanner />
+      {/* <OurTeamImpact/> */}
+      <OurLeadership />
+      <OurTeam />
+      {/* <OurTeamValues /> */}
+      {/* <OurTeamFacility /> */}
+      {/* <OurTeamClients /> */}
+    </>
+  );
 };
 
 export default Team;

280b0b6deb13b5667f6421aa982dfa749ff31302
QaswarAhmad <abdullahhasan20202020@gmail.com>
2024-12-10 21:13:45 +0500

    fixed:img-position

--- src/components/home/AboutRMT.tsx
+++ src/components/home/AboutRMT.tsx
@@ -6,43 +6,39 @@ import Image from "next/image";
 
 function AboutRMT() {
   return (
-    <section className="bg-gray-100 min-h-screen flex flex-col md:flex-row md:justify-center items-start py-10 px-6">
-      <div className="w-full md:w-1/2 relative mb-6 md:mb-0 flex justify-center items-center">
+    <section className="bg-gray-100 min-h-screen flex items-stretch flex-col md:flex-row md:justify-center items-start pt-24 pb-10 px-6">
+      <div className="w-full md:max-w-sm mb-6 md:mb-0 flex justify-end items-center relative h-[490px]">
         {/* First Image */}
-        <div className="w-1/2 my-[50%]">
-          <div className="relative w-[100%]">
-            <Image
-              src="/images/home/rmt_img.jpg"
-              alt="Picture of the rmt"
-              layout="responsive"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover shadow-xl w-96"
-            />
+        <Image
+          src="/images/home/rmt_img.jpg"
+          alt="Picture of the rmt"
+          // layout="responsive"
+          width={300}
+          height={800}
+          className="rounded-lg  shadow-xl w-80 h-[450px]"
+        />
 
-            {/* Second Image positioned over the first image at the left corner */}
-            <div className="absolute top-[-60px] left-[-90px] w-[50%] md:w-[60%]">
-              <Image
-                src="/images/home/rmt_img.jpg"
-                alt="Overlay image"
-                layout="responsive"
-                width={500}
-                height={500}
-                className="rounded-lg object-cover shadow-xl border-4"
-              />
-            </div>
-          </div>
+        {/* Second Image */}
+        <div className="absolute top-16 left-10 transform translate-x-[-50%] translate-y-[-50%] bg-gray-100 p-2 rounded-lg">
+          <Image
+            src="/images/home/rmt_img.jpg"
+            alt="Overlay Picture"
+            // layout="intrinsic"
+            width={250}
+            height={500}
+            className="shadow-lg rounded-lg"
+          />
         </div>
       </div>
 
       <div className="w-full md:w-1/2 p-6 text-left flex flex-col justify-center items-start space-y-6">
         <motion.h1
-          className="text-2xl md:text-5xl font-bold text-[#023550] tracking-[.10em] mb-4"
+          className="text-2xl md:text-5xl font-bold text-[#023550] tracking-[.10em] mb-2"
           initial={{ opacity: 0, y: -50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
         >
-          <span className="block text-base md:text-2xl uppercase mb-4">
+          <span className="block text-base md:text-2xl uppercase mb-2">
             About RMT
           </span>
           <span className="text-[#023550] mb-2">Innovating Ideas,</span>

1ca12b2bad576a5a657b7bc11a7a83d1e6406369
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-12-14 18:13:52 +0500

    Unnecessary Files Deleted

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,24 +1,12 @@
 import React from "react";
 import OurLeadership from "@/components/team/OurLeadership";
 import OurProfessionals from "@/components/team/OurProfessionals";
-// import TeamBanner from "@/components/team/TeamBanner";
-
-// import OurTeamClients from "@/components/team/OurTeamClients";
-// import OurTeamFacility from "@/components/team/OurTeamFacility";
-// import OurTeamImpact from "@/components/team/OurTeamImpact";
-// import OurTeamValues from "@/components/team/OurTeamValues";
-// import TeamAbout from "@/components/team/TeamAbout";
 
 const Team: React.FC = () => {
   return (
     <>
-      {/* <TeamBanner /> */}
-      {/* <OurTeamImpact/> */}
       <OurLeadership />
       <OurProfessionals />
-      {/* <OurTeamValues /> */}
-      {/* <OurTeamFacility /> */}
-      {/* <OurTeamClients /> */}
     </>
   );
 };

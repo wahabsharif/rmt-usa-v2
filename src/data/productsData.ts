e6af19ce8639b3e4766b5662640fe3dcc6367879
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-12-15 20:04:17 +0500

    Products Data

---
+++ src/data/productsData.ts
@@ -0,0 +1,137 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "Invasive Products",
+    slug: "invasive",
+    description:
+      "Our team at Revive Medical Technologies has decades of experience in developing and manufacturing various invasive medical devices such as drug eluting coronary stents, coronary bare metal stents, coronary angioplasty balloon catheters, intracranial stents, intracranial balloon catheters, diagnostic angiography catheters, hernia implants, etc. For this category of medical devices, we offer our clients complete technology development and transfer solution. We also offer technical support solutions to industries looking to establish or improve manufacturing process lines for these medical devices.",
+    shortDescription:
+      "These are medical devices that are introduced into the human body either through a break in the skin or an opening. These are further divided into the following:",
+    images: [
+      "/images/products/invasive-products.jpeg",
+      "/images/products/invasive-products-1.jpg",
+      "/images/products/invasive-products-2.jpg",
+      "/images/products/invasive-products-3.jpg",
+    ],
+    keyFeature: [
+      "Implantable medical devices which are surgically / medically introduced into the body and are intended remain inside the body after the procedure.",
+      "Non-implantable medical devices which are temporarily introduced into the body surgically / medically and are intended to be removed before completion of the procedure.",
+    ],
+  },
+  {
+    id: 2,
+    title: "Non-Invasive Products",
+    slug: "non-invasive",
+    description:
+      "Our technical team comprising of scientists and engineers have ample experience of developing this category of medical devices. For this category of medical devices, we offer our clients complete technology development and transfer solution.",
+    shortDescription:
+      "These are medical devices that fall in either of the following:",
+    images: [
+      "/images/products/non-invasive.jpg",
+      "/images/products/non-invasive-1.jpg",
+      "/images/products/non-invasive-2.jpeg",
+      "/images/products/non-invasive-3.jpg",
+    ],
+    keyFeature: [
+      "Devices that superficially come in contact with the human body surface (like skin)",
+      "Devices that act as external channeling / storage conduits for blood, tissue, body liquids, gases, etc.",
+    ],
+  },
+  {
+    id: 3,
+    title: "Wearable Medical Devices",
+    slug: "wearable-medical-devices",
+    description:
+      "At Revive Medical Technologies, we are leaders in the development of innovative wearable medical devices that empower individuals to take control of their health. With a relentless commitment to innovation, our expertise lies in creating wearable solutions that seamlessly merge advanced technology, precision engineering, and medical expertise. Our team’s deep understanding of both hardware and software allows us to craft wearable medical devices that not only monitor vital health metrics but also offer user-friendly, reliable, and life-improving experiences. By combining the strength of our technical prowess and a dedication to improving healthcare, we are at the forefront of shaping the future of wearable medical devices, transforming the way patients and healthcare professionals manage health and well-being.",
+    shortDescription:
+      "Here are some of the devices we have experience in developing:",
+    images: [
+      "/images/products/wearable-medical-devices.jpg",
+      "/images/products/wearable-medical-devices-1.jpg",
+      "/images/products/wearable-medical-devices-2.webp",
+      "/images/products/wearable-medical-devices-3.jpg",
+      "/images/products/wearable-medical-devices-4.jpg",
+    ],
+    keyFeature: [
+      "Smartwatches for Health Monitoring",
+      "Wearable ECG Monitors",
+      "Continuous Glucose Monitors (CGM)",
+      "Wearable Respiratory Monitors",
+      "Fitness Trackers",
+      "Smart Garments",
+      "Hearing Aid Wearables",
+      "Wearable Pain Management Devices",
+      "Wearable Stress Monitors",
+      "Wearable Medication Delivery Systems",
+      "Fall Detection Wearables",
+    ],
+  },
+  {
+    id: 4,
+    title: "Active Medical Devices",
+    slug: "active-medical-devices",
+    description:
+      "Coupled with our extensive experience in the field, we specialize in the development of active medical devices. With years of expertise under our belt, our seasoned team excels in designing and manufacturing a wide range of advanced medical devices. Our collective knowledge and innovative approach ensure the creation of cutting-edge solutions that meet the evolving demands of the healthcare industry",
+    shortDescription:
+      "Here are some of the devices we have experience in developing:",
+    images: [
+      "/images/products/active-medical-devices.jpeg",
+      "/images/products/active-medical-devices-1.jpg",
+      "/images/products/active-medical-devices-2.jpg",
+      "/images/products/active-medical-devices-3.jpg",
+    ],
+    keyFeature: [
+      "Smart Implantable Medical Devices",
+      "Robotic Surgical Systems",
+      "Remote Patient Monitoring Devices",
+      "Infusion Pumps and Drug Delivery Systems",
+      "Ventilators and Respiratory Support Devices",
+      "Diagnostic Imaging Equipment",
+      "Patient Monitoring Equipment",
+      "Hemodialysis Machines",
+      "Prosthetic and Orthopedic Devices",
+      "Remote Medical Diagnostic Tools",
+    ],
+  },
+  {
+    id: 5,
+    title: "Bio-Compatible Scaffolds, Composites & Coatings",
+    slug: "bio-compatible-scaffolds-composites-coatings",
+    description:
+      "Our team possesses extensive experience in pioneering a diverse range of state-of-the-art solutions within the biomedical materials domain. This experience encompasses the development of advanced surface coatings, drug coatings, and biocompatible tissue engineering scaffolds using innovative methodologies like electrospinning and spin coating. These materials are meticulously crafted to expedite the healing process for both bone and wound injuries. In the realm of surface coatings, we’ve successfully engineered hydrophilic coatings for catheters and graphite coatings for biomedical implants. Furthermore, we’ve formulated drug-loaded coatings for stents, incorporating both synthetic and natural compounds. Additionally, the RMT team has demonstrated exceptional proficiency in developing biocompatible, particulate-reinforced, and fiber-reinforced polymeric composites customized for diverse biomedical applications, underscoring our unwavering dedication to advancing the frontiers of medical technology.",
+    shortDescription: "",
+    images: [
+      "/images/products/bio-compatible-scaffolds-composites-coatings.png",
+      "/images/products/bio-compatible-scaffolds-composites-coatings-1.png",
+      "/images/products/bio-compatible-scaffolds-composites-coatings-2.jpeg",
+      "/images/products/bio-compatible-scaffolds-composites-coatings-3.png",
+      "/images/products/bio-compatible-scaffolds-composites-coatings-4.jpg",
+    ],
+    keyFeature: [],
+  },
+  {
+    id: 6,
+    title: "Wellness Devices",
+    slug: "wellness-devices",
+    description:
+      "With a track record of successful innovation spanning several years, our team brings a wealth of experience to the realm of wellness device development. We have consistently demonstrated our expertise in designing, prototyping, and bringing to market a diverse range of wellness devices that cater to the ever-evolving needs of health-conscious individuals. Our commitment to excellence is evident in our ability to create user-friendly, technologically advanced solutions that empower users to lead healthier lives. By integrating our extensive experience with cutting-edge technology, we stand at the forefront of wellness device development, transforming the way people approach their health and well-being.",
+    shortDescription:
+      "Here are some of the devices we have experience in developing:",
+    images: [
+      "/images/products/wellness-devices.jpg",
+      "/images/products/wellness-devices-1.jpg",
+      "/images/products/wellness-devices-2.jpg",
+      "/images/products/wellness-devices-3.jpg",
+    ],
+    keyFeature: [
+      "Nutritional Health Monitors",
+      "Mental Health and Stress Management Tools",
+      "Hydration Monitoring Devices",
+      "Temperature and Fever Detection Devices",
+      "Sleep Improvement Gadgets",
+      "Personalized Wellness Apps",
+      "Digital Health Coaching Tools",
+      "Fitness and Activity Monitoring Platforms",
+    ],
+  },
+];

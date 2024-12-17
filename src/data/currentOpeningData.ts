fe84409ff3817cd7befb1c69f5671ff91d20c6d7
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-12-17 18:30:32 +0500

    current Opening Data

--- src/data/currentOpeningData.ts
+++ src/data/currentOpeningData.ts
@@ -27,4 +27,39 @@ export const currentOpeningData = [
     jobDescription:
       "Help drive our company's growth by identifying new business opportunities and building strong relationships with clients. Proven experience in business development and excellent communication skills are required.",
   },
+  {
+    id: 5,
+    title: "Mechanical Engineer",
+    location: "Remote / On-Site",
+    jobDescription:
+      "Design and develop mechanical systems and products. Proficiency in CAD software, as well as a strong understanding of materials, thermodynamics, and mechanical design, is essential.",
+  },
+  {
+    id: 6,
+    title: "DevOps Engineer",
+    location: "Remote / On-Site",
+    jobDescription:
+      "Ensure smooth integration and deployment processes. Expertise in cloud platforms, CI/CD pipelines, and containerization (Docker, Kubernetes) is required.",
+  },
+  {
+    id: 7,
+    title: "Electrical Engineer",
+    location: "Remote / On-Site",
+    jobDescription:
+      "Design, develop, and maintain electrical systems. Proficiency in circuit design, power distribution, and electrical safety is necessary for this role.",
+  },
+  {
+    id: 8,
+    title: "Firmware Developer",
+    location: "Remote / On-Site",
+    jobDescription:
+      "Develop low-level software that interacts with hardware components. Expertise in embedded systems programming, C/C++, and hardware integration is required.",
+  },
+  {
+    id: 9,
+    title: "Biomaterial Scientist",
+    location: "Remote / On-Site",
+    jobDescription:
+      "Research and develop biomaterials for various applications. Expertise in materials science, biological interactions, and laboratory techniques is essential for advancing sustainable solutions in healthcare and biotechnology.",
+  },
 ];

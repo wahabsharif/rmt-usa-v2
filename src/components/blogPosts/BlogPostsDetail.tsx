162773fea23529663421e9ca1863fe3a2e9f1a2f
Muhammad Wahab Sharif <www.rex19@gmail.com>
2025-01-03 17:40:01 +0500

    API

--- src/components/blogPosts/BlogPostsDetail.tsx
+++ src/components/blogPosts/BlogPostsDetail.tsx
@@ -18,7 +18,7 @@ const BlogPostsDetail = () => {
     const fetchBlogPost = async () => {
       try {
         const response = await axios.get(
-          `${process.env.API_URL}api/blogs/slug/${slug}`
+          `${process.env.NEXT_PUBLIC_API_URL}api/blogs/slug/${slug}`
         );
         setBlogPost(response.data);
       } catch (error) {
@@ -54,7 +54,7 @@ const BlogPostsDetail = () => {
       <div className="max-w-4xl mx-auto flex-1">
         <h2 className="text-xl md:text-3xl font-bold mb-4">{blogPost.title}</h2>
         <Image
-          src={`${process.env.IMAGE_URL}/${blogPost.featured_image}`}
+          src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/${blogPost.featured_image}`}
           alt={blogPost.title}
           width={800}
           height={400}
--- src/components/newsEvents/NewsEventsList.tsx
+++ src/components/newsEvents/NewsEventsList.tsx
@@ -15,7 +15,9 @@ const NewsEventsList = ({ cards }: { cards: string | number }) => {
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await axios.get(`${process.env.API_URL}api/events`);
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}api/events`
+        );
         setEvents(response.data);
       } catch (err) {
         console.error("Error fetching events data:", err);
@@ -62,7 +64,7 @@ const NewsEventsList = ({ cards }: { cards: string | number }) => {
             >
               {/* Event Image */}
               <Image
-                src={`${process.env.IMAGE_URL}/${newsEvent.image}`}
+                src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/${newsEvent.image}`}
                 alt={newsEvent.title}
                 width={100}
                 height={100}

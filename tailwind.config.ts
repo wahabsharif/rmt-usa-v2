d081449317d51e32ed6255b40678eb9fff5cb66d
Muhammad Wahab Sharif <www.rex19@gmail.com>
2025-01-13 15:59:38 +0500

    production bug fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,64 +8,49 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			sans: [
-  				'SF Pro',
-  				'Manrope',
-  				'Helvetica Neue',
-  				'Arial',
-  				'sans-serif'
-  			]
-  		},
-  		colors: {
-  			thDarkBlue: '#013550',
-  			thLightBlue: '#00b6c6',
-  			thRed: '#DC3332',
-  			white: '#f9fafb',
-  			black: '#030712',
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		boxShadow: {
-  			'3xl': '0 20px 50px rgba(0, 0, 0, 0.5)'
-  		},
-  		keyframes: {
-  			marquee: {
-  				from: {
-  					transform: 'translateX(0)'
-  				},
-  				to: {
-  					transform: 'translateX(calc(-100% - var(--gap)))'
-  				}
-  			},
-  			'marquee-vertical': {
-  				from: {
-  					transform: 'translateY(0)'
-  				},
-  				to: {
-  					transform: 'translateY(calc(-100% - var(--gap)))'
-  				}
-  			},
-  			'marquee-vertical': {
-  				from: {
-  					transform: 'translateY(0)'
-  				},
-  				to: {
-  					transform: 'translateY(calc(-100% - var(--gap)))'
-  				}
-  			}
-  		},
-  		animation: {
-  			marquee: 'marquee var(--duration) infinite linear',
-  			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite',
-  			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      fontFamily: {
+        sans: ["SF Pro", "Manrope", "Helvetica Neue", "Arial", "sans-serif"],
+      },
+      colors: {
+        thDarkBlue: "#013550",
+        thLightBlue: "#00b6c6",
+        thRed: "#DC3332",
+        white: "#f9fafb",
+        black: "#030712",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      boxShadow: {
+        "3xl": "0 20px 50px rgba(0, 0, 0, 0.5)",
+      },
+      keyframes: {
+        marquee: {
+          from: {
+            transform: "translateX(0)",
+          },
+          to: {
+            transform: "translateX(calc(-100% - var(--gap)))",
+          },
+        },
+        "marquee-vertical": {
+          from: {
+            transform: "translateY(0)",
+          },
+          to: {
+            transform: "translateY(calc(-100% - var(--gap)))",
+          },
+        },
+      },
+      animation: {
+        marquee: "marquee var(--duration) infinite linear",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
 } satisfies Config;
